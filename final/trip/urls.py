dc32cb55c5d42f429f2fcc1988d47f7d2a1438f9
Geonyeong <renmsts@naver.com>
2023-10-18 13:54:49 +0900

    add allConnectView

---
+++ final/trip/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'trip'
+
+urlpatterns = [
+    path('',views.index, name = "index"),
+    path('about/',views.about, name='about'),
+    path('blog/',views.blog, name='blog'),
+    path('contact/',views.contact, name='contact'),
+    path('elements/',views.elements, name='elements'),
+    path('main/',views.main, name='main'),
+    path('packages/',views.packages, name='packages'),
+    path('single_blog/',views.single_blog, name='single_blog'),
+]
--- final/trip/views.py
+++ final/trip/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def blog(request):
+    return render(request, 'blog.html')
+def contact(request):
+    return render(request, 'contact.html')
+def elements(request):
+    return render(request, 'elements.html')
+def main(request):
+    return render(request, 'main.html')
+def packages(request):
+    return render(request, 'packages.html')
+def single_blog(request):
+    return render(request, 'single-blog.html')
