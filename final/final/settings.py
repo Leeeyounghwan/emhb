10dd48c082b4174a47da1a4a6980caebb907d0fd
Youngmanbo <yo900326@gmail.com>
2023-10-18 17:30:40 +0900

    fix 앱 위치 변경

--- final/final/settings.py
+++ final/final/settings.py
@@ -36,13 +36,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "daphne",
+    "trip",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "trip",
 ]
 
 MIDDLEWARE = [
