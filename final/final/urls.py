187d69d960d715a53f19a903c9c49fdb08c72fdb
Leeeyounghwan <lee.yhwan226@gmail.com>
2023-10-18 17:27:48 +0900

    feat: 관리자페이지 구현
    관리자 페이지 html, css, js 생성
    
    Resolves: #3

--- final/final/urls.py
+++ final/final/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("trip.urls")),
+    path('favicon.ico', RedirectView.as_view(url='/static/trip_app/favicon.ico', permanent=True)),
 ]
