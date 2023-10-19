51a24c0d77ae5acc03add60872b587b2b647dec9
moon <moonj96@gmail.com>
2023-10-19 20:01:29 +0900

    feat: <문정/ 로그인, 회원가입 구현>

--- final/trip/forms.py
+++ final/trip/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Package
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class Package(forms.ModelForm):
   class Meta:
     model = Package
+    exclude = ['created_at','updated_at']
     fields = ['destination', 'title', 'price', 'image', 'content', 'start_date', 'end_date', 'created_at', 'updated_at']
+
+class UserForm(UserCreationForm):
+
+  class Meta:
+    model = User
+    fields= ('username', 'password1', 'password2')
--- final/trip/views.py
+++ final/trip/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import UserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -62,10 +67,42 @@ def comment(request):
 
 # 로그인, 회원가입 페이지 by 문정
 def user_login(request):
-    return render(request, 'login.html')
+    # if request.method == 'POST':
+    #     form = AuthenticationForm(request=request, data=request.POST)
+    #     if form.is_valid():
+    #         login(request, form.get_user())
+    #         return redirect('trip:main')  # 로그인 성공 시 리디렉션할 페이지
+    # else:
+    #     form = AuthenticationForm()
+    # return render(request, 'login.html', {'form': form})
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request,'main.html')
+    #     else:
+    #         return render(request,'login.html', {'error':'username or password is incorrect'})
+    # else:
+    return render(request,'login.html')
+
+
 
 def user_logout(request):
     redirect('trip:main')
 
 def register(request):
-    return render(request, 'register.html')
+    # return render(request, 'register.html')
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('trip:main')
+    else:
+        form = UserForm()
+    return render(request, 'register.html', {'form': form})
