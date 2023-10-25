1dc44608f7e5f9ed4d5d1dd412b2b60e58daf3cc
Youngmanbo <yo900326@gmail.com>
2023-10-25 12:20:24 +0900

    fix requirment 순서 수정 및 settings login redirect 경로 설정

--- final/final/settings.py
+++ final/final/settings.py
@@ -140,6 +140,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'trip.User'
 
+#로그인 안될시 리다이렉트
+LOGIN_URL = '/login/'
+
 # 실시간 채팅 설정
 
 ASGI_APPLICATION = "final.asgi.application"
