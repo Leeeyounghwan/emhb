dc8b933d938b5cd1251a42f4847307e11abb248b
rara0316 <duf1qudtls2@gmail.com>
2023-10-25 09:24:34 +0900

    feat : add <이수현/models.py> 모델추가에 따른 관리자 페이지 추가

--- final/trip/admin.py
+++ final/trip/admin.py
@@ -1,6 +1,6 @@
 # Register your models here.
 from django.contrib import admin
-from .models import User, TogetherPost, TogetherComment, Schedule,ScheduleComment, TravelDestination, ChoicePost, Voucher, Package
+from .models import User, TogetherPost, TogetherComment, Schedule,ScheduleComment, TravelDestination, ChoicePost, Voucher, Package, Community
 
 admin.site.register(User)
 admin.site.register(TogetherPost)
@@ -11,3 +11,4 @@ admin.site.register(TravelDestination)
 admin.site.register(ChoicePost)
 admin.site.register(Voucher)
 admin.site.register(Package)
+admin.site.register(Community)
