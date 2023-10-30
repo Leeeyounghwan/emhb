a8c32435a623d0c82e079eb5a2c2cddc87a1403f
rara0316 <duf1qudtls2@gmail.com>
2023-10-30 17:41:15 +0900

    feat : add <이수현/admin.py> 관리자페이지

--- final/trip/admin.py
+++ final/trip/admin.py
@@ -2,6 +2,15 @@
 from django.contrib import admin
 from .models import User, TogetherPost, TogetherComment, Schedule,ScheduleComment, TravelDestination, ChoicePost, Voucher, Package, Community
 
+class NoticeAdmin(admin.ModelAdmin):
+  Package = (
+    'title',
+    'content'
+    'writer'
+    'created_at',
+  )
+  search_fields = ('title', 'content', 'writer__user_id')
+
 admin.site.register(User)
 admin.site.register(TogetherPost)
 admin.site.register(TogetherComment)
